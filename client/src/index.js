e96fdc69bca5ac86b4e14148b5a698a6c4d558ad
Javi <rotegras@gmail.com>
2020-02-20 14:10:36 +0100

    add login

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Route, Redirect } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
